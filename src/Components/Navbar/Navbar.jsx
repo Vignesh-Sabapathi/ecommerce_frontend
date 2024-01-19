13f94c05cc8068e113d4569c835fd4fe5c5958f9
Vignesh S <vigneshsabapathi3@gmail.com>
2024-01-19 07:32:30 +0530

    First Change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,11 @@
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
-import { ShopContext } from '../../Context/ShopContext'
 
 const Navbar = () => {
 
   let [menu,setMenu] = useState("shop");
-  const {getTotalCartItems} = useContext(ShopContext);
 
   return (
     <div className='nav'>
@@ -18,16 +15,16 @@ const Navbar = () => {
       </Link>
       <ul className="nav-menu">
         <li onClick={()=>{setMenu("shop")}}><Link to='/' style={{ textDecoration: 'none' }}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}><Link to='/mens' style={{ textDecoration: 'none' }}>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}><Link to='/womens' style={{ textDecoration: 'none' }}>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link to='/kids' style={{ textDecoration: 'none' }}>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("memorials")}}><Link to='/memorials' style={{ textDecoration: 'none' }}>Memorials</Link>{menu==="memorials"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("sculptures")}}><Link to='/sculptures' style={{ textDecoration: 'none' }}>Sculptures</Link>{menu==="sculptures"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("artifacts")}}><Link to='/artifacts' style={{ textDecoration: 'none' }}>Artifacts</Link>{menu==="artifacts"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("about")}}><Link to='/about' style={{ textDecoration: 'none' }}>About</Link>{menu==="about"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
         {localStorage.getItem('auth-token')
         ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace("/");}}>Logout</button>
         :<Link to='/login' style={{ textDecoration: 'none' }}><button>Login</button></Link>}
-        <Link to="/cart"><img src={cart_icon} alt="cart"/></Link>
-        <div className="nav-cart-count">{getTotalCartItems()}</div>
+        
       </div>
     </div>
   )
